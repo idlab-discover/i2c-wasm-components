9ce440029527a6799002e55ef1fefff39e09be48
Zelzahn <friedrich@friedrichvdb.be>
2023-12-20 15:32:06 +0100

    move to host

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,6 +1,4 @@
-use hd44780_driver::{Cursor, CursorBlink, Display, DisplayMode, HD44780};
-use rppal::hal::Delay;
-use rppal::i2c::I2c;
+use  crate::my::project::types::{i2c_init, write};
 
 wit_bindgen::generate!({
     path: "../wit",
@@ -17,25 +15,11 @@ struct Component;
 const I2C_ADDRESS: u8 = 0x27;
 
 impl Guest for Component {
-    fn write() {
-        let i2c = I2c::new().unwrap();
-        let mut delay = Delay::new();
-        let mut lcd = HD44780::new_i2c(i2c, I2C_ADDRESS, &mut delay).unwrap();
+    fn start() -> Result<(), ()> {
+       let _ = i2c_init(I2C_ADDRESS);
+       let _ = write("Hello, world!");
 
-        // Unshift display and set cursor to 0
-        lcd.reset(&mut delay).unwrap();
-        // Clear screen
-        lcd.clear(&mut delay).unwrap();
-        lcd.set_display_mode(
-            DisplayMode {
-                display: Display::On,
-                cursor_visibility: Cursor::Visible,
-                cursor_blink: CursorBlink::On,
-            },
-            &mut delay,
-        ).unwrap();
-
-        lcd.write_str(&message(), &mut delay).unwrap();
+       Ok(())
     }
 }
 
--- host/src/main.rs
+++ host/src/main.rs
@@ -1,8 +1,14 @@
+use crate::my::project::types;
+
 use wasmtime::component::{Component, Linker};
 use wasmtime::{Config, Engine, Store};
 use wasmtime_wasi::preview2::{Table, WasiCtx, WasiView, WasiCtxBuilder};
 use futures::executor::block_on;
 use async_trait::async_trait;
+
+use hd44780_driver::{Cursor, CursorBlink, Display, DisplayMode, HD44780, bus};
+use rppal::hal::Delay;
+use rppal::i2c::I2c;
 
 // Imports will be async functions through #[async_trait] and exports
 // are also invoked as async functions. Requires `Config::async_support`
@@ -14,15 +20,42 @@ wasmtime::component::bindgen!({
 });
 
 struct MyState {
-    name: String,
+    lcd: Option<HD44780<bus::I2CBus<I2c>>>,
+    delay: Option<Delay>,
     table: Table,
     wasi: WasiCtx
 }
 
 #[async_trait]
-impl I2cAppImports for MyState {
-    async fn message(&mut self) -> wasmtime::Result<String> {
-        Ok(self.message.clone())
+impl types::Host for MyState {
+    async fn i2c_init(&mut self, address: u8) -> std::result::Result<std::result::Result<u32, u32>, wasmtime::Error> {
+        let i2c = I2c::new().unwrap();
+        let mut delay = Delay::new();
+        let mut lcd = HD44780::new_i2c(i2c, address, &mut delay).unwrap();
+
+        // Unshift display and set cursor to 0
+        lcd.reset(&mut delay).unwrap();
+        // Clear screen
+        lcd.clear(&mut delay).unwrap();
+        lcd.set_display_mode(
+            DisplayMode {
+                display: Display::On,
+                cursor_visibility: Cursor::Visible,
+                cursor_blink: CursorBlink::On,
+            },
+            &mut delay,
+        ).unwrap();
+
+        self.lcd = Some(lcd);
+        self.delay = Some(delay);
+
+        Ok(Ok(1))
+    }
+    
+    async fn write(&mut self, message: String) -> std::result::Result<std::result::Result<u32, u32>, wasmtime::Error> {
+        self.lcd.as_mut().expect("lcd is not initiated").write_str(&message, self.delay.as_mut().expect("lcd is not initiated")).unwrap();
+
+        Ok(Ok(1))
     }
 }
 
@@ -65,7 +98,7 @@ fn main() -> wasmtime::Result<()> {
     // Bind wasi commmand world
     wasmtime_wasi::preview2::command::add_to_linker(&mut linker)?;
     // Binding host
-    HelloWorld::add_to_linker(&mut linker, |state: &mut MyState| state)?;
+    I2cApp::add_to_linker(&mut linker, |state: &mut MyState| state)?;
 
     // As with the core wasm API of Wasmtime instantiation occurs within a
     // `Store`. The bindings structure contains an `instantiate` method which
@@ -84,16 +117,17 @@ fn main() -> wasmtime::Result<()> {
     let mut store = Store::new(
         &engine,
         MyState {
-            message: "Goeiemorgen!".to_string(),
+            lcd: None,
+            delay: None,
             table,
             wasi
         },
     );
 
     block_on(async {
-        let (bindings, _) = HelloWorld::instantiate_async(&mut store, &component, &linker).await?;
+        let (bindings, _) = I2cApp::instantiate_async(&mut store, &component, &linker).await?;
 
-        bindings.call_write(&mut store).await?;
+        let _ = bindings.call_start(&mut store).await?;
 
         Ok::<(), anyhow::Error>(())
     })?;
