3c74728650e7ad7032f66beac428ef4a759950b5
Zelzahn <friedrich@friedrichvdb.be>
2024-02-15 10:17:28 +0100

    switch to cargo component

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,18 +1,14 @@
-wit_bindgen::generate!({
-    path: "../wit",
-    world: "i2c-app",
-    exports: {
-        world: GuestComponent
-    }
-});
+mod bindings;
 
-struct GuestComponent;
+use bindings::{Guest, host};
+
+struct Component;
 
 
 // This address is the default for the used i2c interface
 const I2C_ADDRESS: u8 = 0x27;
 
-impl Guest for GuestComponent {
+impl Guest for Component {
     fn start() -> Result<(), ()> {
        let _ = host::i2c_init(I2C_ADDRESS);
        let _ = host::write("Hello, world!");
