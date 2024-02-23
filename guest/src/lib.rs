2fd81cf140311f86e3b214d593e856046df11c40
Zelzahn <friedrich@friedrichvdb.be>
2024-02-23 13:10:26 +0100

    switch to sketch:embedded

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,18 +1,41 @@
 mod bindings;
 
-use bindings::Guest;
-use crate::bindings::my::project::types::{i2c_init, write};
-
-// This address is the default for the used i2c interface
-const I2C_ADDRESS: u8 = 0x27;
+use crate::bindings::exports::sketch::embedded::run::Guest;
+use crate::bindings::sketch::embedded::delay::Delay;
+use crate::bindings::sketch::embedded::i2c::I2c;
 
 struct Component;
 
-impl Guest for Component {
-    fn start() -> Result<(), ()> {
-       let _ = i2c_init(I2C_ADDRESS);
-       let _ = write("Hello, world!");
+const ADDRESS: u16 = 0x27;
+const BACKLIGHT: u8 = 0b0000_1000;
+const ENABLE: u8 = 0b0000_0100;
+const REGISTER_SELECT: u8 = 0b0000_0001;
 
-       Ok(())
+fn write_nibble(connection: &mut I2c, nibble: u8, data: bool, delay: &mut Delay) {
+    let rs = match data {
+        false => 0u8,
+        true => REGISTER_SELECT,
+    };
+    let byte = nibble | rs | BACKLIGHT;
+
+    let _ = connection.write(ADDRESS, &[byte, byte | ENABLE]);
+    delay.delay_ns(2_000_000);
+    let _ = connection.write(ADDRESS, &[byte]);
+}
+
+fn write(connection: &mut I2c, byte: u8, data: bool, delay: &mut Delay) {
+    let upper_nibble = byte & 0xF0;
+    write_nibble(connection, upper_nibble, data, delay);
+
+    let lower_nibble = (byte & 0x0F) << 4;
+    write_nibble(connection, lower_nibble, data, delay);
+}
+
+impl Guest for Component {
+    fn run(mut connection: I2c, mut delay: Delay) {
+        for &b in "hello world".as_bytes() {
+            write(&mut connection, b, true, &mut delay);
+            delay.delay_ns(100_000);
+        }
     }
 }
