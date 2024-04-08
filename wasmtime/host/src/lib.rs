b75b05e399c11a7e0bdfcc48412d5aa4c352570b
Zelzahn <friedrich@friedrichvdb.be>
2024-04-08 15:03:03 +0200

    wasmtime benchmarking

---
+++ wasmtime/host/src/lib.rs
@@ -0,0 +1,25 @@
+mod device;
+
+use wasmtime::{
+    component::{Component, Linker},
+    Config, Engine, Result,
+};
+use wasmtime_wasi::WasiCtxBuilder;
+
+pub fn execute() -> Result<String, anyhow::Error> {
+    let engine = Engine::new(Config::new().wasm_component_model(true))?;
+
+    let mut linker = Linker::new(&engine);
+
+    // Bind wasi commmand world
+    wasmtime_wasi::command::sync::add_to_linker(&mut linker)?;
+
+    let wasi = WasiCtxBuilder::new()
+        .inherit_stdout()
+        .inherit_stdio()
+        .build();
+
+    let component = Component::from_file(&engine, "hat.wasm")?;
+
+    Ok(device::run(linker, engine, component, wasi)?)
+}
--- wasmtime/host/src/main.rs
+++ wasmtime/host/src/main.rs
@@ -1,27 +1,5 @@
-mod device;
-
-use wasmtime::{
-    component::{Component, Linker},
-    Config, Engine, Result,
-};
-use wasmtime_wasi::WasiCtxBuilder;
-
 fn main() -> Result<(), anyhow::Error> {
-    let engine = Engine::new(Config::new().wasm_component_model(true))?;
-
-    let mut linker = Linker::new(&engine);
-
-    // Bind wasi commmand world
-    wasmtime_wasi::command::sync::add_to_linker(&mut linker)?;
-
-    let wasi = WasiCtxBuilder::new()
-        .inherit_stdout()
-        .inherit_stdio()
-        .build();
-
-    let component = Component::from_file(&engine, "hat.wasm")?;
-
-    let res = device::run(linker, engine, component, wasi)?;
+    let res = host::execute()?;
 
     println!("{:?}", res);
 
